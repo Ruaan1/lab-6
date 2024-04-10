ae2239719842189b6660f1d3d9f32769d522efae
Ruaan1 <2446659@students.wits.ac.za>
2024-04-10 17:15:37 +0200

    test 7

---
+++ api/DeleteCar/index.js
@@ -0,0 +1,7 @@
+const cars = require('../cars.json');
+module.exports = async function (context, req) {
+    const id = req.params.id;
+    const index = cars.findIndex(car => car.id === id);
+    cars.splice(index, 1);
+    context.res.json(cars);
+}
--- src/script.js
+++ src/script.js
@@ -62,7 +62,7 @@ carForm.addEventListener('submit', event => {
 // Function to remove a car
 function removeCar(index) {
     const carId = cars[index].id;
-    fetch(`/cars/${carId}`, {
+    fetch(`https://agreeable-pond-06fe23f03.5.azurestaticapps.net/api/cars/${carId}`, {
         method: 'DELETE'
     })
         .then(response => response.json())
